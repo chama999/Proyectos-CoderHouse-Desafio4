ca5195da63747ea77fe10d0e426fd3e2de9aca5c
chama999 <83312190+chama999@users.noreply.github.com>
2021-11-23 00:09:42 -0300

    Add files via upload

--- Desafio4/main.js
+++ Desafio4/main.js
@@ -1,8 +1,17 @@
+const productos = require('./contenedor.js')
 const express = require('express')
 const app = express()
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 8080)
 
-app.get('/saludo', (request, response) => {
-    response.send('<h2>Hola Mundo</h2>')
+app.get('/productos', (req, res) => {
+    let listaProductos = productos.getAllObjects()
+    res.json(listaProductos)
 })
+app.get('/productoRandom', (req, res) => {
+    let listaProductos = productos.getAllObjects()
+    //obtener un producto aleatorio de listaProductos
+    let productoAleatorio = listaProductos[Math.floor(Math.random() * listaProductos.length)]   
+    res.json(productoAleatorio)
+})
+
 
